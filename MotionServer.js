6987f277f9e569641527f291a4724b26ac1035d8
Artur Zelek <1a.artur.zelek@gmail.com>
2024-11-26 09:51:14 +0100

    Update MotionServer.js

--- MotionServer.js
+++ MotionServer.js
@@ -5,22 +5,19 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on("connection", (ws) => {
   console.log("Klient podłączony");
 
-  // Odbieranie wiadomości od klienta (np. aplikacji mobilnej)
   ws.on("message", (message) => {
     const messageString = message.toString();
     console.log("Odebrano wiadomość od klienta:", messageString);
 
     try {
-      const data = JSON.parse(messageString); // Przekształcenie stringu w obiekt JSON
+      const data = JSON.parse(messageString);
 
-      // Przykład struktury danych: { type: "motion", accelX, accelY, accelZ, gyroX, gyroY, gyroZ }
       if (data.type === "motion") {
         console.log("Odebrane dane ruchu:", data);
 
-        // Wysyłanie danych do wszystkich połączonych klientów
         wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data)); // Wysyłanie danych do klienta
+            client.send(JSON.stringify(data));
           }
         });
       } else {
@@ -31,7 +28,6 @@ wss.on("connection", (ws) => {
     }
   });
 
-  // Obsługa rozłączenia klienta
   ws.on("close", () => {
     console.log("Klient rozłączony");
   });
